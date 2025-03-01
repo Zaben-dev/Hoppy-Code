8c5385c8903d52bffcd67aaa04b4fcf1c338a731
Patryk Dmitruk <patrykdmitruk227@gmial.com>
2025-03-01 01:24:32 +0100

    add shared workspace package

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Post } from '@hoppy-code/types';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app';
+  post!: Post;
 }
